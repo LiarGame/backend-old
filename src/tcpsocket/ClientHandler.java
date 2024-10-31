eb617a8174ddd5a41c38ceb329fa678b65d83bab
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-10-31 11:54:58 +0900

    feat(ClientHandler): 클라이언트의 요청을 받는 기본 구조 생성

--- src/tcpsocket/ClientHandler.java
+++ src/tcpsocket/ClientHandler.java
@@ -1,4 +1,38 @@
 package tcpsocket;
 
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private final Socket socket;
+    private BufferedReader br;
+    private PrintWriter pw;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+
+            String request = null;
+            while ((request = br.readLine()) != null) {
+                System.out.println("요청: " + request);
+                //TODO: 요청을 받아서 실행하는 로직 구현 필요
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) br.close();
+                if (pw != null) pw.close();
+                if (socket != null) socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
